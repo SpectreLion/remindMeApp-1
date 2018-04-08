7ba8bca7d2b7eabac3fb700301d5d7338d647955
mike18G <migueguevara3@gmail.com>
2018-04-08 10:04:15 -0500

    Creación de Arrays para cada Categoria Actualizado.
    Se elimino el campo "category" de todos los arrays.

--- src/staticData/friendsTasks.js
+++ src/staticData/friendsTasks.js
@@ -1,7 +1,7 @@
 export default [
-  { id: 1, title: 'Estreno Infinity War', completed: false, category: 4, date: 1518449400000 },
-  { id: 2, title: 'Promo Alitas Wingman', completed: false, category: 4, date: 1518362400000 },
-  { id: 3, title: 'Campamento Tapalpa', completed: false, category: 4, date: 1518846500000 },
-  { id: 4, title: 'Campus Party', completed: false, category: 4, date: 15184700000 },
-  { id: 5, title: 'Comic Con', completed: false, category: 4, date: 1517342600000 }
+  { id: 1, title: 'Estreno Infinity War', completed: false, date: 1518449400000 },
+  { id: 2, title: 'Promo Alitas Wingman', completed: false, date: 1518362400000 },
+  { id: 3, title: 'Campamento Tapalpa', completed: false, date: 1518846500000 },
+  { id: 4, title: 'Campus Party', completed: false, date: 15184700000 },
+  { id: 5, title: 'Comic Con', completed: false, date: 1517342600000 }
 ];
--- src/staticData/workTasks.js
+++ src/staticData/workTasks.js
@@ -1,7 +1,7 @@
 export default [
-  { id: 1, title: 'Junta SCRUM', completed: false, category: 1, date: 1518743100000 },
-  { id: 2, title: 'Desarrollar App', completed: false, category: 1, date: 1518432400000 },
-  { id: 3, title: 'Planear Día', completed: false, category: 1, date: 1517659800000 },
-  { id: 4, title: 'Diseñar Mocks', completed: false, category: 1, date: 1518434500000 },
-  { id: 5, title: 'Junta con equipo de Dise', completed: false, category: 1, date: 1518657400000 }
+  { id: 1, title: 'Junta SCRUM', completed: false, date: 1518743100000 },
+  { id: 2, title: 'Desarrollar App', completed: false, date: 1518432400000 },
+  { id: 3, title: 'Planear Día', completed: false, date: 1517659800000 },
+  { id: 4, title: 'Diseñar Mocks', completed: false, date: 1518434500000 },
+  { id: 5, title: 'Junta con equipo de Dise', completed: false, date: 1518657400000 }
 ];
