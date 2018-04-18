d904f8cc0642bfac085d35aed71f72c1557dc44d
SpectreLion <jose.tin96@hotmail.com>
2018-04-17 19:19:13 -0500

    .

--- src/modals/AddTaskModal.js
+++ src/modals/AddTaskModal.js
@@ -146,7 +146,7 @@ const styles = StyleSheet.create({
     fontFamily: 'Avenir',
     textAlign: 'center',
     fontSize: 17,
-    color: 'white'
+    color: Colors.white
   },
   cancelButton: {
       width: 325,
