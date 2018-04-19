8454fbfa3bf3034b92b13227581386016784e9c8
JP <jpv1124@gmail.com>
2018-04-19 10:13:47 -0500

    Se implementaron los estilos dentro de los componentes task y tadkheader, se creo una clase para poder obtener la fecha en el formato deseado.

---
+++ src/items/DateFormat.js
@@ -0,0 +1,23 @@
+import constants from './../staticData/Constants';
+
+export default class Helper{
+
+  formatMonth(date){
+    const targetDate = new Date(date),
+          month = constants.months[targetDate.getMonth()];
+
+
+
+    return `${month}`;
+  }
+
+  formatDay(date){
+    const targetDate = new Date(date),
+          day = targetDate.getDate();
+
+
+    return `${day}`;
+  }
+
+
+}
